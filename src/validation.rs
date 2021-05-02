8506c11e73403bae9b358bda1266ae8fc3fc61b6
Aoife <aoifeebradley@gmail.com>
2021-05-03 00:34:12 +0200

    Add the match certificate function

--- src/validation.rs
+++ src/validation.rs
@@ -1,5 +1,5 @@
-use anyhow::{anyhow, Result};
-use ring::{digest, hmac::verify};
+use anyhow::Result;
+use ring::digest;
 use rustls::{Certificate, RootCertStore, ServerCertVerified, ServerCertVerifier, TLSError};
 use std::{
     collections::HashMap,
@@ -10,19 +10,58 @@ use webpki::DNSNameRef;
 use x509_parser::{certificate::X509Certificate, parse_x509_certificate, time::ASN1Time};
 pub struct StoredCertificate {
     pub fingerprint: String,
-    pub algorithm: String,
+    pub not_after: i64,
 }
+#[derive(Default)]
 pub struct CertificateMap {
-    pub stored: HashMap<String, StoredCertificate>,
+    pub stored_certificates: HashMap<String, StoredCertificate>,
     path_to_file: PathBuf,
 }
 
 impl CertificateMap {
     pub fn new(path: &Path) -> Self {
+        if path.exists() {
+            CertificateMap::load_from_file(path);
+        }
         Self {
-            stored: HashMap::new(),
+            stored_certificates: HashMap::new(),
             path_to_file: path.to_owned(),
         }
+    }
+    fn load_from_file(path: &Path) -> Self {
+        let file = std::fs::read_to_string(path).expect("File could not be loaded!");
+        let mut map = HashMap::new();
+        for line in file.lines() {
+            let values = line.splitn(3, ';').collect::<Vec<&str>>();
+            let (host, fingerprint, not_after) = (
+                values[0].to_string(),
+                values[1].to_string(),
+                values[2]
+                    .parse::<i64>()
+                    .expect("unable to parse the expiry time of saved certificate!"),
+            );
+            map.insert(
+                host.to_lowercase(),
+                StoredCertificate {
+                    fingerprint,
+                    not_after,
+                },
+            );
+        }
+        return Self {
+            stored_certificates: map,
+            path_to_file: path.to_owned(),
+        };
+    }
+    fn save_to_file(&self) {
+        let mut contents = String::new();
+        for (host, cert) in &self.stored_certificates {
+            contents.push_str(&format!(
+                "{};{};{}\n",
+                host, cert.fingerprint, cert.not_after
+            ));
+        }
+        std::fs::write(&self.path_to_file, contents).expect("unable to save certificates to store!")
     }
     fn verify(
         &mut self,
@@ -31,9 +70,10 @@ impl CertificateMap {
         der_parsed_cert: &X509Certificate,
     ) -> Result<ServerCertVerified, TLSError> {
         let hostname = hostname.to_lowercase();
-        if !Self::verify_expiry(&hostname, der_parsed_cert) {
-            return Err(TLSError::WebPKIError(webpki::Error::CertExpired));
-        }
+        CertificateMap::verify_expiry(der_parsed_cert)?;
+        der_parsed_cert
+            .verify_signature(None)
+            .map_err(|_| TLSError::WebPKIError(webpki::Error::InvalidSignatureForPublicKey))?;
 
         // TODO: check against certificate we've saved if they exists for this hostname.
 
@@ -41,11 +81,34 @@ impl CertificateMap {
         let fingerprint = ring::digest::digest(&digest::SHA256, der_encoded_cert);
         Ok(ServerCertVerified::assertion())
     }
-    fn verify_expiry(_hostname: &str, cert: &X509Certificate) -> bool {
-        if !cert.validity().is_valid_at(ASN1Time::now()) {
-            return false;
+    fn verify_expiry(parsed_cert: &X509Certificate) -> Result<(), TLSError> {
+        if !parsed_cert.validity().is_valid_at(ASN1Time::now()) {
+            return Err(TLSError::WebPKIError(webpki::Error::CertExpired));
         }
-        cert.verify_signature(None).is_ok()
+        Ok(())
+    }
+    fn match_certificate(
+        &mut self,
+        hostname: &str,
+        new_fingerprint: &[u8],
+        new_cert: &X509Certificate,
+    ) {
+        if let Some(cert) = self.stored_certificates.get_mut(hostname) {
+            // TODO: invert conditions!!
+            // unwrap because it will always be valid
+            if std::str::from_utf8(new_fingerprint).unwrap() != cert.fingerprint {
+                // if not, is the stored certificate expired?
+                if ASN1Time::now() >= ASN1Time::from_timestamp(cert.not_after) {
+                    // update stored cert with new certificate
+                    *cert = StoredCertificate {
+                        fingerprint: std::str::from_utf8(new_fingerprint).unwrap().to_string(),
+                        not_after: new_cert.validity().not_after.timestamp(),
+                    };
+                } else {
+                    // TODO: show the user a warning! this certificate might be dodgy
+                }
+            }
+        }
     }
 }
 pub struct TOFUVerifier {
@@ -73,7 +136,7 @@ impl ServerCertVerifier for TOFUVerifier {
             .map_err(|_| TLSError::WebPKIError(webpki::Error::BadDER))?;
         self.map
             .lock()
-            .map_err(|_| TLSError::General("Unable to Lock!".to_string()))?
+            .map_err(|_| TLSError::General("unable to lock certificate map mutex!".to_string()))?
             .verify(AsRef::<str>::as_ref(&hostname.to_owned()), en_cert, &cert)
     }
 }
